5046cb05be4a1ce721a28c661b46cacf573f464e
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-01-22 10:15:25 +0530

    update1

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,12 +1,14 @@
 function ListGroup() {
+  const items = ["New York", "Tokio", "San Fransisco", "London", "Paris"];
+
+  //JSX Markup
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {items.map((items) => (
+        <li key={items}>{items}</li>
+      ))}
+    </>
   );
 }
 
