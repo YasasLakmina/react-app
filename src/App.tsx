2aaef2ebcf197205b6908de756f2fb517effd700
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-01-23 10:29:26 +0530

    commit2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import ListGroup from "./Components/ListGroup";
 
-function App(){
-  return <div><ListGroup></ListGroup></div>;
+function App() {
+  let items = ["New York", "Tokio", "San Fransisco", "London", "Paris"];
+
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities"></ListGroup>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,13 +1,47 @@
-function ListGroup() {
-  const items = ["New York", "Tokio", "San Fransisco", "London", "Paris"];
+import { MouseEvent, useState } from "react";
 
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
+  //State Hook - this will tell react this component has a data that can change over time
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  //Event Handler
+  // const handleClick = (event: MouseEvent) => console.log(event);
+
+  //Method 1
+  const getMessage = () => {
+    return items.length === 0 ? <p>No items in the List</p> : null;
+  };
   //JSX Markup
   return (
     <>
-      <h1>List</h1>
-      {items.map((items) => (
-        <li key={items}>{items}</li>
-      ))}
+      <h1>{heading}</h1>
+      {getMessage()}
+      {/*Method 2
+        x && y 
+        if x is true it will return y
+        if x is false it will return false
+      */}
+      {items.length === 0 && <p>No items in the List</p>}
+      <ul className="list-group">
+        {items.map((items, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item "
+            }
+            onClick={() => setSelectedIndex(index)}
+            key={items}
+          >
+            {items}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
