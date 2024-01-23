2eaf6a3f1120b274d0fc15fb352dc75e0ced131f
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-01-23 14:53:46 +0530

    commit4

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 //import ListGroup from "./Components/ListGroup";
-import Alert from "./Components/Alert";
+// import Alert from "./Components/Alert";
+
+import Button from "./Components/Button";
 
 function App() {
   return (
     <div>
-      <Alert>Hello World</Alert>
+      <Button onClick={() => console.log("Clicked")}>My Button</Button>
     </div>
   );
 }
+
+// function App() {
+//   return (
+//     <div>
+//       <Alert>Hello <span>World</span></Alert>
+//     </div>
+//   );
+// }
 
 // function App() {
 //   let items = ["New York", "Tokio", "San Fransisco", "London", "Paris"];
---
+++ src/Components/Button.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  children: string;
+  color?: "primary" | "secondary" | "danger";
+  onClick: () => void;
+}
+
+const Button = ({ children, onClick, color = "primary" }: Props) => {
+  return (
+    <button className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
