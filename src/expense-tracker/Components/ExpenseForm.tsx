410a0fa14dbe67f053f3b693b92ed072fd57e459
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-02-05 06:29:49 +0530

    commit9

--- src/expense-tracker/Components/ExpenseForm.tsx
+++ src/expense-tracker/Components/ExpenseForm.tsx
@@ -82,7 +82,7 @@ const ExpenseForm = ({ onSubmit }: Props) => {
           <p className="text-danger">{errors.category.message}</p>
         )}
       </div>
-      <button className="btn-btn-primary">Submit</button>
+      <button className="btn btn-primary">Submit</button>
     </form>
   );
 };
