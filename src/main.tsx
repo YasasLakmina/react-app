3d009e8c7dff85d8c024904ec1a512094ea8a638
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-02-10 04:54:11 +0530

    commit2

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./App.css";
-// import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
